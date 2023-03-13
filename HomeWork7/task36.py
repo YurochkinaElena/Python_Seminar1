44919aec36b0f72179dbdc193e0c5d1989629d58
YurochkinaElena <116380568+YurochkinaElena@users.noreply.github.com>
2023-03-13 10:38:32 +0300

    Update HomeWork7/task36.py
    Co-authored-by: Danil <49741194+samodurOFF@users.noreply.github.com>

--- HomeWork7/task36.py
+++ HomeWork7/task36.py
@@ -20,7 +20,8 @@
 def print_operation_table(operation, num_rows=6, num_columns=6):
     for i in range(1, num_rows + 1):
         for j in range(1, num_columns + 1):
-            print(str(operation(i, j)).ljust(3), end=' ')
+            result = str(operation(i, j))
+            print(result.rjust(len(str(operation(num_rows, j))) + 1), end="")
         print()
 
 print_operation_table(lambda x, y: x * y)
